dc8f57e9e114f23e1e43b6b2f4bf162da900837b
kebri <kebridon.nemat.life@gmail.com>
2021-03-06 12:35:44 +0500

    basic models first draft

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,302 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.genre_name
+
+    class Meta:
+        verbose_name = "genre"
+        verbose_name_plural = "genres"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    url = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Actor(models.Model):
+    actor_name = models.CharField(max_length=50)
+    actor_birthday = models.DateField(default=date.today)
+    # actor_age = date.today-actor_birthday
+    # Perhaps any other fields are required
+
+    def __str__(self):
+        return self.actor_name
+
+    class Meta:
+        verbose_name = "Actor"
+        verbose_name_plural = "Actors"
+
+
+class Movie(models.Model):
+    COUNTRY = (
+        ('Afghanistan', 'Afghanistan'),
+        ('Albania', 'Albania'),
+        ('Algeria', 'Algeria'),
+        ('Andorra', 'Andorra'),
+        ('Angola', 'Angola'),
+        ('Antigua and Barbuda', 'Antigua and Barbuda'),
+        ('Argentina', 'Argentina'),
+        ('Armenia', 'Armenia'),
+        ('Australia', 'Australia'),
+        ('Austria', 'Austria'),
+        ('Azerbaijan', 'Azerbaijan'),
+        ('The Bahamas', 'The Bahamas'),
+        ('Bahrain', 'Bahrain'),
+        ('Bangladesh', 'Bangladesh'),
+        ('Barbados', 'Barbados'),
+        ('Belarus', 'Belarus'),
+        ('Belgium', 'Belgium'),
+        ('Belize', 'Belize'),
+        ('Benin', 'Benin'),
+        ('Bhutan', 'Bhutan'),
+        ('Bolivia', 'Bolivia'),
+        ('Bosnia and Herzegovina', 'Bosnia and Herzegovina'),
+        ('Botswana', 'Botswana'),
+        ('Brazil', 'Brazil'),
+        ('Brunei', 'Brunei'),
+        ('Bulgaria', 'Bulgaria'),
+        ('Burkina Faso', 'Burkina Faso'),
+        ('Burundi', 'Burundi'),
+        ('Cabo Verde', 'Cabo Verde'),
+        ('Cambodia', 'Cambodia'),
+        ('Cameroon', 'Cameroon'),
+        ('Canada', 'Canada'),
+        ('Central African Republic', 'Central African Republic'),
+        ('Chad', 'Chad'),
+        ('Chile', 'Chile'),
+        ('China', 'China'),
+        ('Colombia', 'Colombia'),
+        ('Comoros', 'Comoros'),
+        ('Congo, Democratic Republic of the', 'Congo, Democratic Republic of the'),
+        ('Congo, Republic of the', 'Congo, Republic of the'),
+        ('Costa Rica', 'Costa Rica'),
+        ('Côte d’Ivoire', 'Côte d’Ivoire'),
+        ('Croatia', 'Croatia'),
+        ('Cuba', 'Cuba'),
+        ('Cyprus', 'Cyprus'),
+        ('Czech Republic', 'Czech Republic'),
+        ('Denmark', 'Denmark'),
+        ('Djibouti', 'Djibouti'),
+        ('Dominica', 'Dominica'),
+        ('Dominican Republic', 'Dominican Republic'),
+        ('East Timor (Timor-Leste),', 'East Timor (Timor-Leste),'),
+        ('Ecuador', 'Ecuador'),
+        ('Egypt', 'Egypt'),
+        ('El Salvador', 'El Salvador'),
+        ('Equatorial Guinea', 'Equatorial Guinea'),
+        ('Eritrea', 'Eritrea'),
+        ('Estonia', 'Estonia'),
+        ('Eswatini', 'Eswatini'),
+        ('Ethiopia', 'Ethiopia'),
+        ('Fiji', 'Fiji'),
+        ('Finland', 'Finland'),
+        ('France', 'France'),
+        ('Gabon', 'Gabon'),
+        ('The Gambia', 'The Gambia'),
+        ('Georgia', 'Georgia'),
+        ('Germany', 'Germany'),
+        ('Ghana', 'Ghana'),
+        ('Greece', 'Greece'),
+        ('Grenada', 'Grenada'),
+        ('Guatemala', 'Guatemala'),
+        ('Guinea', 'Guinea'),
+        ('Guinea-Bissau', 'Guinea-Bissau'),
+        ('Guyana', 'Guyana'),
+        ('Haiti', 'Haiti'),
+        ('Honduras', 'Honduras'),
+        ('Hungary', 'Hungary'),
+        ('Iceland', 'Iceland'),
+        ('India', 'India'),
+        ('Indonesia', 'Indonesia'),
+        ('Iran', 'Iran'),
+        ('Iraq', 'Iraq'),
+        ('Ireland', 'Ireland'),
+        ('Israel', 'Israel'),
+        ('Italy', 'Italy'),
+        ('Jamaica', 'Jamaica'),
+        ('Japan', 'Japan'),
+        ('Jordan', 'Jordan'),
+        ('Kazakhstan', 'Kazakhstan'),
+        ('Kenya', 'Kenya'),
+        ('Kiribati', 'Kiribati'),
+        ('Korea, North', 'Korea, North'),
+        ('Korea, South', 'Korea, South'),
+        ('Kosovo', 'Kosovo'),
+        ('Kuwait', 'Kuwait'),
+        ('Kyrgyzstan', 'Kyrgyzstan'),
+        ('Laos', 'Laos'),
+        ('Latvia', 'Latvia'),
+        ('Lebanon', 'Lebanon'),
+        ('Lesotho', 'Lesotho'),
+        ('Liberia', 'Liberia'),
+        ('Libya', 'Libya'),
+        ('Liechtenstein', 'Liechtenstein'),
+        ('Lithuania', 'Lithuania'),
+        ('Luxembourg', 'Luxembourg'),
+        ('Madagascar', 'Madagascar'),
+        ('Malawi', 'Malawi'),
+        ('Malaysia', 'Malaysia'),
+        ('Maldives', 'Maldives'),
+        ('Mali', 'Mali'),
+        ('Malta', 'Malta'),
+        ('Marshall Islands', 'Marshall Islands'),
+        ('Mauritania', 'Mauritania'),
+        ('Mauritius', 'Mauritius'),
+        ('Mexico', 'Mexico'),
+        ('Micronesia, Federated States of', 'Micronesia, Federated States of'),
+        ('Moldova', 'Moldova'),
+        ('Monaco', 'Monaco'),
+        ('Mongolia', 'Mongolia'),
+        ('Montenegro', 'Montenegro'),
+        ('Morocco', 'Morocco'),
+        ('Mozambique', 'Mozambique'),
+        ('Myanmar (Burma),', 'Myanmar (Burma),'),
+        ('Namibia', 'Namibia'),
+        ('Nauru', 'Nauru'),
+        ('Nepal', 'Nepal'),
+        ('Netherlands', 'Netherlands'),
+        ('New Zealand', 'New Zealand'),
+        ('Nicaragua', 'Nicaragua'),
+        ('Niger', 'Niger'),
+        ('Nigeria', 'Nigeria'),
+        ('North Macedonia', 'North Macedonia'),
+        ('Norway', 'Norway'),
+        ('Oman', 'Oman'),
+        ('Pakistan', 'Pakistan'),
+        ('Palau', 'Palau'),
+        ('Panama', 'Panama'),
+        ('Papua New Guinea', 'Papua New Guinea'),
+        ('Paraguay', 'Paraguay'),
+        ('Peru', 'Peru'),
+        ('Philippines', 'Philippines'),
+        ('Poland', 'Poland'),
+        ('Portugal', 'Portugal'),
+        ('Qatar', 'Qatar'),
+        ('Romania', 'Romania'),
+        ('Russia', 'Russia'),
+        ('Rwanda', 'Rwanda'),
+        ('Saint Kitts and Nevis', 'Saint Kitts and Nevis'),
+        ('Saint Lucia', 'Saint Lucia'),
+        ('Saint Vincent and the Grenadines', 'Saint Vincent and the Grenadines'),
+        ('Samoa', 'Samoa'),
+        ('San Marino', 'San Marino'),
+        ('Sao Tome and Principe', 'Sao Tome and Principe'),
+        ('Saudi Arabia', 'Saudi Arabia'),
+        ('Senegal', 'Senegal'),
+        ('Serbia', 'Serbia'),
+        ('Seychelles', 'Seychelles'),
+        ('Sierra Leone', 'Sierra Leone'),
+        ('Singapore', 'Singapore'),
+        ('Slovakia', 'Slovakia'),
+        ('Slovenia', 'Slovenia'),
+        ('Solomon Islands', 'Solomon Islands'),
+        ('Somalia', 'Somalia'),
+        ('South Africa', 'South Africa'),
+        ('Spain', 'Spain'),
+        ('Sri Lanka', 'Sri Lanka'),
+        ('Sudan', 'Sudan'),
+        ('Sudan, South', 'Sudan, South'),
+        ('Suriname', 'Suriname'),
+        ('Sweden', 'Sweden'),
+        ('Switzerland', 'Switzerland'),
+        ('Syria', 'Syria'),
+        ('Taiwan', 'Taiwan'),
+        ('Tajikistan', 'Tajikistan'),
+        ('Tanzania', 'Tanzania'),
+        ('Thailand', 'Thailand'),
+        ('Togo', 'Togo'),
+        ('Tonga', 'Tonga'),
+        ('Trinidad and Tobago', 'Trinidad and Tobago'),
+        ('Tunisia', 'Tunisia'),
+        ('Turkey', 'Turkey'),
+        ('Turkmenistan', 'Turkmenistan'),
+        ('Tuvalu', 'Tuvalu'),
+        ('Uganda', 'Uganda'),
+        ('Ukraine', 'Ukraine'),
+        ('United Arab Emirates', 'United Arab Emirates'),
+        ('United Kingdom', 'United Kingdom'),
+        ('United States', 'United States'),
+        ('Uruguay', 'Uruguay'),
+        ('Uzbekistan', 'Uzbekistan'),
+        ('Vanuatu', 'Vanuatu'),
+        ('Vatican City', 'Vatican City'),
+        ('Venezuela', 'Venezuela'),
+        ('Vietnam', 'Vietnam'),
+        ('Yemen', 'Yemen'),
+        ('Zambia', 'Zambia'),
+        ('Zimbabwe', 'Zimbabwe'),
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    published_date = models.PositiveSmallIntegerField()
+    world_premiere = models.DateField(default=date.today)
+    country = models.CharField(max_length=100, null=True, blank=True, choices=COUNTRY)
+    genre = models.ManyToManyField(Genre)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    motto = models.CharField(max_length=250, blank=True, null=True)
+    director = models.ManyToManyField(Actor, related_name='Movie_director')
+    starring = models.ManyToManyField(Actor, related_name='Movie_starring')
+    poster = models.ImageField(upload_to="movies/")
+    url = models.SlugField(max_length=150, unique=True)
+    duration = models.FloatField(null=True)
+    budget = models.PositiveIntegerField(default=0)
+    fees_in_usa = models.PositiveIntegerField(default=0)
+    fees_in_world = models.PositiveIntegerField(default=0)
+
+
+class MovieShots(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="movie_shots/")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Movie Shot"
+        verbose_name_plural = "Movie Shots"
+
+
+class Rating(models.Model):
+    value = models.FloatField(default=5.0)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.value} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Movie Rating"
+        verbose_name_plural = "Movie Ratings"
+
+
+class Reviews(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
+    text = models.TextField(max_length=5000)
+    parent = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, blank=True, null=True
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
